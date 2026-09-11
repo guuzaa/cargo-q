6dc3b9f71bd3f8d0335230003efa888515334bab
jowell <guza@duck.com>
2026-09-11 11:23:11 +0800

    chore: print commit hash and release date for version

--- src/cli.rs
+++ src/cli.rs
@@ -7,7 +7,8 @@ use std::io;
 
 #[derive(Parser, Debug)]
 #[command(name = "cargo-q")]
-#[command(version)]
+#[command(version = concat!(env!("CARGO_PKG_VERSION"), " (",
+        env!("GIT_HASH"), " ", env!("GIT_COMMIT_DATE"), ")"))]
 #[command(
     about = "A Cargo subcommand for running multiple Cargo commands sequentially or in parallel."
 )]
